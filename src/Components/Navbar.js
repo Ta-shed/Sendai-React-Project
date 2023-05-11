883ef48863520b5f03a4a7e72762a3a6687f7a00
TZZ <tzvekare@gmail.com>
2023-05-11 23:18:59 +0100

    html for navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,26 @@
+
+const  Navbar = () => {
+    return (
+        <div>
+<div>
+
+
+<nav>
+    <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact Us</a></li>
+    <li><a href="#">Login</a></li>
+    <li><a href="#">Sign Up</a></li>
+    </ul>
+    <li><a href="#" className="button">Send Money</a></li>
+</nav>
+
+</div>
+
+
+        </div>
+    )
+}
+
+export default Navbar;
