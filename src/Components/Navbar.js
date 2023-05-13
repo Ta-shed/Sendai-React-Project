2aac9ed4ed11e01e654f26f842b27011c07ddaa4
TZZ <tzvekare@gmail.com>
2023-05-13 12:07:26 +0100

    styled navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,18 +3,24 @@ const  Navbar = () => {
     return (
         <div>
 <div>
-
-
-<nav>
-    <ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Contact Us</a></li>
-    <li><a href="#">Login</a></li>
-    <li><a href="#">Sign Up</a></li>
+<body>
+<header class="bg-gray-800 text-white">
+<nav className="flex items-center justify-between px-4 py-2">
+    <ul className="flex space-x-4">
+    <li className="hover:underline"><a href="#" className="">Home</a></li>
+    <li className="hover:underline"><a href="#">About</a></li>
+    <li className="hover:underline"><a href="#">Contact Us</a></li>
+    <li className="hover:underline"><a href="#">Login</a></li>
+    <li className="hover:underline"><a href="#">Sign Up</a></li>
     </ul>
-    <li><a href="#" className="button">Send Money</a></li>
+    <li className="list-none float-right"><a href="#" className="bg-green-500 hover:bg-green-600 font-bold py-2 px-4 rounded">Send Money</a></li>
 </nav>
+</header>
+<main class="container mx-auto py-8">
+    
+    
+  </main>
+</body>
 
 </div>
 
