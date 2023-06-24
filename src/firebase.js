2c9e29ea78b5ee631fe4ee3687f6e59a2b2ff0de
TZZ <tzvekare@gmail.com>
2023-06-24 15:21:58 +0100

    added and configured firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtvN8eoaf6g4TWtX4jNIcrl1qrjJi2lXA",
+    authDomain: "sendai-fa871.firebaseapp.com",
+    projectId: "sendai-fa871",
+    storageBucket: "sendai-fa871.appspot.com",
+    messagingSenderId: "606054205448",
+    appId: "1:606054205448:web:f421736af613f6da6f29bd",
+    measurementId: "G-9YGR8WY7HG"
+};
+
+firebase.initializeApp(firebaseConfig);
